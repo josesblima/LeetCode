52ea60869a5643f27dbef7da231a3535c0a4a8ee
josesblima <149493661+josesblima@users.noreply.github.com>
2024-01-17 14:36:25 +0000

    Time: 107 ms (81.40%) | Memory: 18.4 MB (15.63%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=167 lang=python3
+#
+# [167] Two Sum II - Input Array Is Sorted
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, numbers, target: int):
+        left = 0
+        right = len(numbers) - 1
+        while numbers[left] + numbers[right] != target:
+            # Checks if sum is greater than target
+            # and decreases right or increases left
+            if numbers[left] + numbers[right] > target and right > 0:
+                right -= 1
+                continue
+            if numbers[left] + numbers[right] < target and left < len(numbers) - 1:
+                left += 1
+                continue
+            left += 1
+        return [left + 1, right + 1]
+    twoSum(1, [3,24,50,79,88,150,345], 200)
+        
+# @lc code=end
+
