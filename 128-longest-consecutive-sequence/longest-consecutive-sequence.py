a1aac0e61685151d4ab6989796eff36d5edfe33e
josesblima <149493661+josesblima@users.noreply.github.com>
2024-01-17 12:32:43 +0000

    Time: 374 ms (74.78%) | Memory: 48.7 MB (5.03%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -9,10 +9,12 @@ import random
 class Solution:
     def longestConsecutive(self, nums) -> int:
         nums_set = set(nums)
-        print(nums_set)
         res = 0
         left = None
         right = None
+        # Function starts at a random spot and check
+        # for consecutive numbers up and down whilst
+        # removing reached numbers from the set
         def leftRightConsec(nums_set, left, right):
             if left in nums_set:
                 nums_set.remove(left)
@@ -30,17 +32,15 @@ class Solution:
             return left, right
         if len(nums) == 0:
             return 0
+        # This will call the function  at random
+        # keys until it's empty
         while len(nums_set) > 0:
             random_number = nums_set.pop()
             print(random_number)
             left, right = leftRightConsec(nums_set, random_number, random_number)
             if right - left + 1 > res:
                 res = right - left + 1
-
-        print(res)
         return res
-
-
     longestConsecutive(1, [100,4,200,1,3,2])
 # @lc code=end
 
