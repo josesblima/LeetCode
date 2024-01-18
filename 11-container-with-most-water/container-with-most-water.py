22f566e078709f91108b4131d199d3d8e8de61e5
josesblima <149493661+josesblima@users.noreply.github.com>
2024-01-18 19:38:07 +0000

    Time: 512 ms (92.12%) | Memory: 30 MB (36.58%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -10,16 +10,17 @@ class Solution:
         l = 0
         r = len(height) - 1
         res = 0
-        while l  < r:
+        while l < r:
+            # Checks if current area is higher than res
             if min(height[l], height[r]) * (r - l) > res:
                 res = min(height[l], height[r]) * (r - l)
+            # Change one with exhausted potential
             if height[l] <  height[r]:
                 l += 1  
                 continue  
             if height[l] >=  height[r]:
                 r -= 1
                 continue
-        print(res)
         return res
     maxArea(1, [1,3,2,5,25,24,5])
             
