054d437d48dace4bbdc98bd75ccbc39de7b2091d
josesblima <149493661+josesblima@users.noreply.github.com>
2024-02-05 05:44:01 +0000

    Time: 901 ms (58.43%) | Memory: 29.9 MB (96.60%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,20 +1,19 @@
 class Solution:
     def dailyTemperatures(self, temperatures):
+        # We keep track of values in stack until the temperature rises
         stack = [temperatures[0]]
+        # Keep track of the index of each value in the stack
         stack_index = [0]
         for x in range(1, len(temperatures)):
-            counter = 0
+            # Every time the temperature rises, we remove from the end of the stack
             while len(stack) >= 1 and temperatures[x] > stack[-1]:
-                counter += 1
                 popped_stack = stack_index.pop()
                 temperatures[popped_stack] = x - popped_stack
                 stack.pop()
             stack.append(temperatures[x])
             stack_index.append(x)
+        # At the end, there might still be items in the stack, which means
+        # temperature won't ever rise withing the given days, so we change them to 0
         for x in stack_index:
             temperatures[x] = 0
-        return temperatures            
-
-
-    dailyTemperatures(1, [55,38,53,81,61,93,97,32,43,78])
-        
+        return temperatures
