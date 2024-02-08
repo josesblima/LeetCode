84058cfd4d8cdae148d1e32e97516d3683f31203
josesblima <149493661+josesblima@users.noreply.github.com>
2024-02-08 21:11:44 +0000

    Time: 257 ms (87.78%) | Memory: 18.2 MB (58.77%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -3,7 +3,6 @@ class Solution:
     def minEatingSpeed(self, piles, h):
         maximum = max(piles)
         l, r = 0, maximum
-
         while r != l + 1:
             midpoint = (l + r) // 2
             res = 0
@@ -15,8 +14,5 @@ class Solution:
             # Go down
             if res > h:
                 l = midpoint
-        print(l, r)
         return r
-
-
     minEatingSpeed(1, piles = [3,6,7,11], h = 8)
